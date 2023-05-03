27fb7c951f9e53f26db10241c642625b4abca624
Nethsara <nethsarasiyum2@gmail.com>
2023-05-03 19:21:29 +0530

    created mysql db and tables

--- src/app.js
+++ src/app.js
@@ -2,11 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
-var app = express();
-app.use(cors({
-    origin: ["http://localhost:3000"],
-}));
-app.use(express.json());
-app.listen(8000, function () {
-    console.log("Listen on Port 8000");
+var typeorm_1 = require("typeorm");
+var dataSource = new typeorm_1.DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "msc_admin",
+    entities: ["src/entity/*.js"],
+    logging: true,
+    synchronize: true,
+});
+dataSource
+    .initialize()
+    .then(function () {
+    var app = express();
+    app.use(cors({
+        origin: ["http://localhost:3000"],
+    }));
+    app.use(express.json());
+    app.listen(8000, function () {
+        console.log("Listen on Port 8000");
+    });
+})
+    .catch(function (err) {
+    console.error("Error during Data Source initialization", err);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,35 @@
 import * as express from "express";
 import * as cors from "cors";
-
-const app = express();
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
-
-app.use(express.json());
-
-app.listen(8000, () => {
-  console.log("Listen on Port 8000");
+import { DataSource } from "typeorm";
+const dataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "",
+  database: "msc_admin",
+  entities: ["src/entity/*.js"],
+  logging: true,
+  synchronize: true,
 });
+
+dataSource
+  .initialize()
+  .then(() => {
+    const app = express();
+
+    app.use(
+      cors({
+        origin: ["http://localhost:3000"],
+      })
+    );
+
+    app.use(express.json());
+
+    app.listen(8000, () => {
+      console.log("Listen on Port 8000");
+    });
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
